e6ac66d5c76b87ccd485676ef09358eab0c0db98
Sunil-kumar <sunilku.951975@gmail.com>
2025-05-05 19:39:59 +0530

    7nth commit with v-18 @EnableTransactionManagement

--- src/main/java/com/RestFulAPI/services/JournalEntryService.java
+++ src/main/java/com/RestFulAPI/services/JournalEntryService.java
@@ -4,9 +4,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import javax.management.RuntimeErrorException;
+
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.RestFulAPI.entity.JournalEntry;
 import com.RestFulAPI.entity.User;
@@ -21,26 +24,34 @@ public class JournalEntryService {
 	@Autowired
 	private UserService userService;
 
+	@Transactional
 	public void saveJEntry(JournalEntry journalEntry, String Username) {
 
-		User user = userService.findByUserName(Username);
+		try {
 
-		JournalEntry saved = jeRepo.save(journalEntry); //journal entry save ho rahi hai 
-													   // agr yaha exception aagai iska matlab je save ho jayegai but user me add nahi hogi 
-													  // user me un je ki entry nahi aayegai to yah inconsistensy aajegai 	
-		user.getJournalEntries().add(saved);
+			User user = userService.findByUserName(Username);
 
-		userService.saveUEntry(user);
+			JournalEntry saved = jeRepo.save(journalEntry); // journal entry save ho rahi hai
+															// agr yaha exception aagai iska matlab je save ho jayegai
+															// but user me add nahi hogi
+															// user me un je ki entry nahi aayegai to yah inconsistensy
+															// aajegai
+			user.getJournalEntries().add(saved);
+
+			userService.saveUEntry(user);
+
+		} catch (Exception e) {
+				System.out.println(e);
+			// Throw a RuntimeException so that Spring can trigger rollback
+			throw new RuntimeException("An error occurred while saving the entry", e);
+		}
+
 	}
-	
-	
-	public void saveJEntry(JournalEntry journalEntry ) {
 
-	
+	public void saveJEntry(JournalEntry journalEntry) {
 
 		jeRepo.save(journalEntry);
 	}
-	
 
 	public List<JournalEntry> getAllJe() {
 
@@ -54,7 +65,7 @@ public class JournalEntryService {
 	}
 
 	public void deletById(ObjectId id, String userName) {
-		User user =userService.findByUserName(userName);
+		User user = userService.findByUserName(userName);
 		user.getJournalEntries().removeIf(x -> x.getId().equals(id));
 		userService.saveUEntry(user);
 		jeRepo.deleteById(id);
